04d04da66f94c59827297cdc3b39ad4ef9944645
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-27 16:16:05 +0200

    added input and button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 // # Fase di preparazione
 //recupero gli elementi
-let numbersList = document.getElementById('pc-numbers');
+const numbersList = document.getElementById('pc-numbers');
+const userNumber = document.getElementById('user-number');
+const playButton = document.getElementById('play-button');
+//creo l'array di numeri vuoto
+let randomNumbers = [];
+
 
 // # Funzioni
 //genero dei numeri casuali da 1 a 100
@@ -10,20 +15,21 @@ function getNumbers() {
 }
 console.log(getNumbers());
 
-
-// # Genero 5 numeri casuali
-//creo l'array di numeri vuoto
-let randomNumbers = [];
-for (let i = 0; i < 5; i++) {
-    //genero un numero casuale per 5 volte
-    getNumbers(i);
-    //lo inserisco nell'array
-    randomNumbers.push(getNumbers(i));
-    //creo un elemento lista con le classi di bootstraps
-    let listElement = document.createElement('li');
-    listElement.classList.add('list-group-item')
-    //gli inserisco uno dei numeri casuali generati precedentemente
-    listElement.innerText = randomNumbers[i];
-    //inserisco il tutto nella lista html
-    numbersList.appendChild(listElement);
+// Genero 5 numeri e creo lista
+function pcNumbers() {
+    for (let i = 0; i < 5; i++) {
+        //genero un numero casuale per 5 volte
+        getNumbers(i);
+        //lo inserisco nell'array
+        randomNumbers.push(getNumbers(i));
+        //creo un elemento lista con le classi di bootstraps
+        let listElement = document.createElement('li');
+        listElement.classList.add('list-group-item')
+        //gli inserisco uno dei numeri casuali generati precedentemente
+        listElement.innerText = randomNumbers[i];
+        //inserisco il tutto nella lista html
+        numbersList.appendChild(listElement);
+    }
 }
+
+pcNumbers();
